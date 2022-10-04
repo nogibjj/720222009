3c4eeaea546acc02333937e26b460e0f42760461
RuixinLou <lruixin2@bu.edu>
2022-10-04 14:31:50 -0400

    2

---
+++ 720_9.29.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv")
+plt.scatter(data['Mortality rate, infant (per 1,000 live births)'], data["GDP per capita (constant 2010 US$)"])
+plt.show()
+
+
